af212adeb9938b0c6a90a397910004fdf183f029
Francesco <f.cerofolini@open-box.it>
2022-04-04 11:16:32 +0200

    capitolo 5

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Answer;
+use App\Entity\Question;
+use App\Entity\Topic;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -15,7 +24,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
+        return $this->render('admin/index.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -34,15 +44,46 @@ class DashboardController extends AbstractDashboardController
         // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+    public function configureUserMenu (UserInterface $user): UserMenu
+    {
+        if (!$user instanceof User)
+        {
+            throw new \Exception('Wrong user');
+        }
+
+        return parent::configureUserMenu($user)
+            ->setAvatarUrl($user->getAvatarUrl())
+            ->setMenuItems([
+                MenuItem::linkToUrl('My Proile', 'fas fa-user', $this->generateUrl('app_profile_show'))
+            ])
+            ;
+    }
+
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Code Easyadminbundle');
+            ->setTitle('Cauldron Overflow Admin')
+            ->generateRelativeUrls()
+            ;
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+        yield MenuItem::linkToCrud('Questions', 'fa fa-question-circle', Question::class);
+        yield MenuItem::linkToCrud('Answer', 'fa fa-comments', Answer::class);
+        yield MenuItem::linkToCrud('Topic', 'fa fa-folder', Topic::class);
+        yield MenuItem::linkToCrud('User', 'fa fa-users', User::class);
+        yield MenuItem::linkToUrl('Homepage', 'fas fa-home', $this->generateUrl('app_homepage'));
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
+
+    public function configureActions (): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+
 }
